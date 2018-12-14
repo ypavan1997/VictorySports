ce980ea73ab24052a6bd4d42cb05ad7398b1a274
Pushpinder <pushpisingh@paypal.com>
2018-12-14 18:41:43 +0530

    Use single screen for admin/coach addition

---
+++ src/components/AddNewUser.js
@@ -0,0 +1,201 @@
+import React from 'react'
+import Form from "semantic-ui-react/dist/es/collections/Form/Form";
+import Segment from "semantic-ui-react/dist/es/elements/Segment/Segment";
+import Grid from "semantic-ui-react/dist/es/collections/Grid/Grid";
+import Container from "semantic-ui-react/dist/es/elements/Container/Container";
+import Label from "semantic-ui-react/dist/es/elements/Label/Label";
+import Button from "semantic-ui-react/dist/es/elements/Button/Button";
+import Dropdown from "semantic-ui-react/dist/es/modules/Dropdown/Dropdown";
+import DatePicker from "react-datepicker/es/index";
+import TextArea from "semantic-ui-react/dist/es/addons/TextArea/TextArea";
+
+const options = [
+  {key: 'a', value: 'admin', text: 'Admin'},
+  {key: 'c', value: 'coach', text: 'Coach'}
+];
+
+export default class AddNewUser extends React.Component {
+
+  state = { activeItem: 'Add New User' };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  handleUserRoleChange = (event, props) => {
+    this.setState({user_role: props.value})
+  };
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  render() {
+    return <Container>
+      <Segment>
+        <Label  size={'medium'} color={'blue'} attached='top'>New User Details</Label>
+        <Form>
+        <Grid doubling >
+            <Grid.Row>
+              <Grid.Column textAlign={'left'} mobile={6}>
+                <label className={'Admin-Form-Label'}>Name </label>
+              </Grid.Column>
+              <Grid.Column mobile={10}>
+                <Form.Field>
+                  <Form.Input fluid placeholder="Name" />
+                </Form.Field>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={2}>
+              <Grid.Column textAlign={'left'} mobile={6}>
+                <label className={'Admin-Form-Label'}>Username </label>
+              </Grid.Column>
+              <Grid.Column mobile={10}>
+                <Form.Field>
+                  <Form.Input fluid placeholder="Username" />
+                </Form.Field>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column mobile={6} textAlign={'left'}>
+                <label className={'Admin-Form-Label'}>Password </label>
+              </Grid.Column>
+              <Grid.Column mobile={10}>
+                <Form.Field>
+                  <Form.Input type={'password'} fluid placeholder="Password" />
+                </Form.Field>
+              </Grid.Column>
+
+            </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column mobile={6} textAlign={'left'}>
+              <label className={'Admin-Form-Label'}>Retype Password </label>
+            </Grid.Column>
+            <Grid.Column mobile={10}>
+              <Form.Field>
+                <Form.Input type={'password'} fluid placeholder="Retype Password" />
+              </Form.Field>
+            </Grid.Column>
+
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column mobile={6} textAlign={'left'}>
+              <label className={'Admin-Form-Label'}>Role</label>
+            </Grid.Column>
+            <Grid.Column mobile={10}>
+            <Form.Field>
+                <Form.Select options={options} placeholder='Role' onChange={this.handleUserRoleChange} fluid/>
+            </Form.Field>
+            </Grid.Column>
+          </Grid.Row>
+
+          { this.state.user_role === 'coach' &&
+            <React.Fragment>
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Name </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Form.Field>
+                    <Form.Input fluid placeholder="Name" />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Address </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Form.Field>
+                    <TextArea placeholder='Address' style={{ minHeight: 100 }} />
+                  </Form.Field>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Pincode </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Form.Field>
+                    <Form.Input fluid placeholder="Pincode" />
+                  </Form.Field>
+                </Grid.Column>
+
+              </Grid.Row>
+
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Mobile </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Form.Field>
+                    <Form.Input fluid placeholder="Mobile" />
+                  </Form.Field>
+                </Grid.Column>
+
+              </Grid.Row>
+
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Date of birth </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+
+                  <DatePicker
+                    selected={this.state.startDate}
+                    placeholderText="Click to select a date of birth"
+                    maxDate={new Date()}
+                    showDisabledMonthNavigation
+                    showMonthDropdown
+                    showYearDropdown
+                    dropdownMode="select"
+                    dateFormat="dd/MM/yyyy"
+                    onChange={this.handleChange}
+                  />
+
+                </Grid.Column>
+
+              </Grid.Row>
+
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Education </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Form.Field>
+                    <Dropdown placeholder='Education' fluid multiple selection options={[]} />
+                  </Form.Field>
+                </Grid.Column>
+
+              </Grid.Row>
+
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <label className={'Admin-Form-Label'}>Sport </label>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Form.Field>
+                    <Dropdown placeholder='Sport(s)' fluid multiple selection options={[{key: 'a', text: 'Football' ,value: 'football'},
+                      {key: 'b', text: 'Boxing', value: 'boxing'}]} />
+                  </Form.Field>
+                </Grid.Column>
+
+              </Grid.Row>
+            </React.Fragment>
+
+
+
+
+               }
+        </Grid>
+        </Form>
+        <br/>
+        {this.state.user_role === 'coach' && <Button primary content={'Add New Coach'}/> }
+        {this.state.user_role === 'admin' && <Button primary content={'Add New Admin'}/> }
+        {!this.state.user_role && <Button disabled content={'Add New User'}/> }
+      </Segment>
+    </Container>
+  }
+
+}
--- src/components/UserManagementScreen.js
+++ src/components/UserManagementScreen.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react'
-import { Grid, Segment, Container } from 'semantic-ui-react'
-import AddAdmin from "./AddNewAdminUser";
-import Form from "semantic-ui-react/dist/es/collections/Form/Form";
+import AddUser from "./AddNewUser";
 import Breadcrumb from "semantic-ui-react/dist/es/collections/Breadcrumb/Breadcrumb";
-import AddNewCoach from "./AddNewCoach";
+import Segment from "semantic-ui-react/dist/es/elements/Segment/Segment";
 
-const options = [
-  {key: 'a', value: 'admin', text: 'Admin'},
-  {key: 'c', value: 'coach', text: 'Coach'}
-]
 
 const sections = [
   { key: 'user_mgmgt', content: 'User Management', link: false },
@@ -16,13 +10,6 @@ const sections = [
 ]
 
 export default class UserManagementScreen extends Component {
-  state = { activeItem: 'Add New User' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  handleUserRoleChange = (event, props) => {
-    this.setState({user_role: props.value})
-  }
 
   render() {
 
@@ -30,20 +17,9 @@ export default class UserManagementScreen extends Component {
       <Segment basic textAlign={'left'}>
       <Breadcrumb icon='right angle' sections={sections} />
       </Segment>
-    <Grid>
-      <Grid.Row>
-        <Grid.Column>
-          <Form size={'large'}>
-            <Form.Select inline label='Select a role for the user' options={options} placeholder='Role' onChange={this.handleUserRoleChange}/>
-          </Form>
-        </Grid.Column>
-      </Grid.Row>
 
+      <AddUser/>
 
-    </Grid>
-      { this.state.user_role === 'admin' && <AddAdmin/> }
-
-      { this.state.user_role === 'coach' && <AddNewCoach/>}
 
     </React.Fragment>
   }

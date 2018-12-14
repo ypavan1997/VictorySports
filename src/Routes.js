6fe28d8bab9d832443da2786e47ccc4fc3f053fd
Pushpinder <pushpisingh@paypal.com>
2018-12-14 16:36:11 +0530

    Add dummy page for unmatched routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,15 @@
 import { Route, Switch } from 'react-router-dom'
 import UserManagementScreen from './components/UserManagementScreen'
 import React from 'react'
+import UserStatus from "./components/UserStatus";
+import CreateHub from "./components/CreateHub";
+import NoMatch from "./components/NoMatch";
 
 export const Routes = () => {
   return <Switch>
     <Route path='/user_mgmt' component={UserManagementScreen}/>
-    <Route path='/hub_mgmt' component={() => <p>Hub mgmgt</p>}/>
+    <Route path='/user_status' component={UserStatus}/>
+    <Route path='/create_hub' component={CreateHub}/>
+    <Route component={NoMatch} />
   </Switch>;
 };
---
+++ src/components/NoMatch.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const NoMatch = ( ) => <p> Page under development</p>;
+
+
+export default NoMatch
